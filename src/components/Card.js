2bc9cabcd27c41cbfb4cd15fc651861d68fdebab
Ashh21 <ash.shrivastav0@gmail.com>
2023-09-13 01:18:12 +0530

    added validation in form and some css changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,30 @@
 import React from 'react'
 
 
-const Card = ({data}) => {
-   const {name, cardNum, mm, yy, cvv} = data;
+const Card = ({ data }) => {
+    const { name, cardNum, mm, yy, cvv } = data;
     return (
-        <div>
-            <div className='card-div'>
+
+        <div className='card-main'>
+            <div className='card1'>
                 <div className='card-div-1'>
-                    <h2> {name} </h2>
+                    <h2> {cardNum} </h2>
                 </div>
                 <div className='card-div-2'>
                     <div>
-                        <p> {cardNum} </p>
+                        <h2> {name} </h2>
                     </div>
                     <div>
-                        <p>
+                        <h2>
                             {mm} / {yy}
-                        </p>
+                        </h2>
                     </div>
                 </div>
             </div>
             <div className='card2'>
-                <div className='c-div1'>
+                <div className='card2-div1'>
                 </div>
-                <div className='c-div2'>
+                <div className='card2-div2'>
                     <p> {cvv} </p>
                 </div>
             </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,20 +3,54 @@ import { Card } from './Card';
 
 
 const Form = () => {
-    let [name, setName] = useState('');
+    const [name, setName] = useState();
     const [cardNum, setCardNum] = useState('');
     const [mm, setMm] = useState('');
     const [yy, setYy] = useState('');
     const [cvv, setCvv] = useState('');
     const [data, sendData] = useState('');
+    const [errors, setErrors] = useState({
+        name: "", cardNum: "", mm: "",
+        yy: "", cvv: ""
+    })
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("submitted")
-        const newData = { name, cardNum, mm, yy, cvv }
-        sendData(newData);
-        setName(''); setCardNum(''); setMm("");
-        setYy(''); setCvv("")
+        const newErrors = {};
+
+        if (name.length === 0) {
+            newErrors.name = 'Card holder name required';
+        }
+
+        if (cardNum.length < 16 || isNaN(cardNum)) {
+            newErrors.cardNum = ' Card number required';
+        }
+
+        if (!mm) {
+            newErrors.mm = 'month is required';
+        }
+
+        if (!yy) {
+            newErrors.yy = 'year is required';
+        }
+
+        if (isNaN(cvv) || cvv.length < 3) {
+            newErrors.cvv = 'CVV is required'
+        }
+
+
+        if (Object.keys(newErrors).length === 0) {
+            const newData = { name, cardNum, mm, yy, cvv }
+            sendData(newData)
+            setName('');
+            setCardNum('');
+            setMm("");
+            setYy('');
+            setCvv("")
+        }
+        else {
+            setErrors(newErrors)
+        }
     }
 
     return (
@@ -34,33 +68,41 @@ const Form = () => {
                                 setName(e.target.value)
                             }}
                         />
+                        <span className="error">{errors.name}</span>
                     </div>
                     <div className='form-div flex1'>
                         <label>CARD NUMBER </label>
-                        <input className='input' placeholder='e.g. 1234 5678 9123 0000' type='text' value={cardNum}
+                        <input className='input' placeholder='e.g. 1234 5678 9123 0000' type='text'
+                            value={cardNum}
                             onChange={(e) => {
                                 setCardNum(e.target.value)
                             }}
                         />
+                        <span className="error">{errors.cardNum}</span>
                     </div>
+
                     <div className='flex'>
                         <div className='form-div flex1'>
                             <label>EXP.  DATE ( MM / YY)</label>
                             <div className='input-div'>
-                                <div>
-                                    <input className='exp-input' type='text' maxLength={2} placeholder='MM' value={mm}
+                                <div className='exp-input'>
+                                    <input className='exp-input-1' type='text' maxLength={2} placeholder='MM' pattern="(0?[1-9]|1[0-2])" value={mm}
                                         onChange={(e) => {
                                             setMm(e.target.value)
                                         }}
                                     />
+                                    <span className="error">{errors.mm}</span>
                                 </div>
-                                <div>
-                                    <input className='exp-input' type='text' maxLength={2} placeholder='YY' value={yy}
+
+                                <div className='exp-input'>
+                                    <input className='exp-input-1' type='text' maxLength={2} placeholder='YY' value={yy}
                                         onChange={(e) => {
                                             setYy(e.target.value)
                                         }}
                                     />
+                                    <span className="error">{errors.yy}</span>
                                 </div>
+
                             </div>
                         </div>
                         <div className='form-div flex1'>
@@ -70,6 +112,7 @@ const Form = () => {
                                     setCvv(e.target.value)
                                 }}
                             />
+                            <span className="error">{errors.cvv}</span>
                         </div>
                     </div>
                     <div>
@@ -82,3 +125,5 @@ const Form = () => {
 }
 
 export { Form }
+
+// pattern="\d{3,4}"
